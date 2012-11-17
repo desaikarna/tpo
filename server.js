78a991eaec5af2fda74c9e74bbd3bf69a039b7bc
OpenShift guest <r.brian.amesbury@gmail.com>
2012-11-16 22:42:07 -0500

    removing the callback on the listen call

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     response.send('Hello World!');
+    response.r
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});
+app.listen(port);
