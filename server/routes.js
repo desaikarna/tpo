bd66ca95b8e167301c380bc081aa90fc02ae049b
OpenShift guest <r.brian.amesbury@gmail.com>
2012-11-20 01:19:26 -0500

    showing a visual change that core is merged into master

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
   
     app.get('/', function(request, response) {
-        response.send('Hello Core II!');
+        response.send('Hello World II!');
     });
   
 }
