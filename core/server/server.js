bf5283760d6971fe8e181daae532438beaffbc67
OpenShift guest <r.brian.amesbury@gmail.com>
2012-11-20 00:44:58 -0500

    relocating server.js to core/server/server.js

---
+++ core/server/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(request, response) {
+    response.send('Hello World!');
+    response.r
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port);
