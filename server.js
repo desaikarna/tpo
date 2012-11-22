df375977a8d98a3ba49f9b68508220ffe8c09f24
OpenShift guest <r.brian.amesbury@gmail.com>
2012-11-22 10:46:20 -0500

    adding ejs

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var engine = require('ejs-locals')
-var routes = require('./routes/routes.js');
+var routes = require(__dirname +'/routes/routes.js');
 
 var app = express();
 
 app.engine('ejs', engine);
 
-app.set('views', './views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.use("/styles", express.static(__dirname + '/assets/stylesheets'));
+app.use("/js", express.static(__dirname + '/assets/javascript'));
+app.use("/img", express.static(__dirname + '/assets/images'));
 
 routes(app);
 
