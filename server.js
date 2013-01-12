3d7c99c00aa2bf52a3be8a4d9cac948302ad4ffe
R. Brian Amesbury <r.brian.amesbury@gmail.com>
2013-01-12 01:12:49 -0500

    express, angular-ui, creating account works

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
-var node_static = require('node-static');
-var cache = 3600;
-if (!process.env.NODE_ENV){
-    cache = 0;
-}
-var fileServer = new node_static.Server('./root', {cache:cache, serverInfo:'server.thirdplanetout.com'});
-var errorMode = false;
-var errorHandler = function (request, response) {
-    response.writeHead(302, {'Location': 'http://error.thirdplanetout.com'});
-    response.end();   
-};
+var express = require('express');
+var app = express();
+var fs = require('fs');
 
-require('http').createServer(function (request, response) {
-    if (errorMode) {
-        errorHandler(request, response);    
-    } else {
-        request.addListener('end', function () {
-            fileServer.serve(request, response, function (error, result) {
-                if (error){
-                    console.error("WARN: url=" + request.url + " status=" + error.status + " message=" + error.message);
-                    if (error.status === 404) {
-                        fileServer.serveFile('/not-found.html', 404, {}, request, response);
-                    } else {
-                        errorMode = true;
-                        errorHandler(request, response);
-                    }
-                }
-            });
-        });
-    }
-}).listen(process.env.PORT);
+var file = fs.readFileSync('./root/not-found.html');
+
+app.use(express.static('root'));
+app.use(express.bodyParser());
+
+require('./request/request.js')(app);
+
+app.get('*', function(request, response){
+    response.writeHeader(404, {"Content-Type": "text/html"});  
+    response.write(file);  
+    response.end();
+});
+
+app.listen(process.env.PORT);
