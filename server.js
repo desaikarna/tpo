2915d7d5c999a29fc0629a3648c4da9fe1c4dc69
R. Brian Amesbury <ramesbury@familysearch.org>
2013-08-01 22:15:49 -0600

    updating package.json adding mustache

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var mu2Express = require("mu2Express");
 
 var file = fs.readFileSync('./root/not-found.html');
 
-app.use(express.static('root'));
+app.use(function (request, response, next) {
+    var HTTPStatus = { OK : { CODE : 200 } }
+    if(request.headers["user-agent"] && (request.headers["user-agent"]).indexOf("Wgeet") === 0) {
+        response.json(HTTPStatus.OK.CODE);
+    } else {
+        next();
+    }
+});
+app.use(function (request, response, next) {
+    var HTTPStatus = { OK : { CODE : 200 } }
+    if(request.headers["user-agent"] && (request.headers["user-agent"]).indexOf("curl") === 0) {
+        response.json(HTTPStatus.OK.CODE);
+    } else {
+        next();
+    }
+});
 app.use(express.bodyParser());
 app.use(express.cookieParser());
 
-app.get('/heartbeat', function(reqquest, response){
+app.engine('mustache', mu2Express.engine);
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/root');
+
+app.get('/heartbeat', function(request, response){
 	response.json(200, {});
+});
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        'locals' : {
+            'context' : ''
+        }
+    });
 });
 
 require('./request/request.js')(app);
